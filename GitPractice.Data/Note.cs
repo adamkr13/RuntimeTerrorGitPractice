f376bf2ee9d0e1b10645636e1985dea4a99c99f1
Caleb Addie <cjaddie.89@gmail.com>
2021-02-16 18:10:20 -0500

    One ring to rule them all.

--- GitPractice.Data/Note.cs
+++ GitPractice.Data/Note.cs
@@ -23,5 +23,7 @@ namespace GitPractice.Data
         public DateTimeOffset? ModifiedUtc { get; set; }
 
         //hello
+
+        // "One Ring to rule them all, One Ring to find them, One Ring to bring them all, and in the darkness bind them."
     }
 }
